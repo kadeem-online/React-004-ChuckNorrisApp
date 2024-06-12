31e9398d69d05376ab1580e8241dc6a4d3bc0fbd
kadeem254 <kadeemalexander254@gmail.com>
2024-06-12 21:21:16 +0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import HomePage from "./pages/home/index";
+
+export default function App() {
+	return (
+		<div
+			id="app-container"
+			className="w-full min-h-screen relative bg-slate-100 text-xl"
+		>
+			<HomePage />
+		</div>
+	);
+}
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+	return (
+		<main>
+			<section id="display-section" className="py-10">
+				<div className="container mx-auto">
+					<h1 className="font-semibold text-slate-800 text-5xl ">
+						Chuck Norris Jokes
+					</h1>
+				</div>
+			</section>
+		</main>
+	);
+}
