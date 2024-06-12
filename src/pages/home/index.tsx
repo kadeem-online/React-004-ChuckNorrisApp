189b095c4bb7c52ef55839413291e248f06c0086
kadeem254 <kadeemalexander254@gmail.com>
2024-06-12 22:40:03 +0300

    UPDATE: added boilerplate for th HomePage component content

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,10 +6,33 @@ export default function HomePage({ className }: HomePageParameters) {
 	return (
 		<main className={className || ``}>
 			<section id="display-section" className="py-10">
-				<div className="container mx-auto">
-					<h1 className="font-semibold text-slate-800 text-5xl">
+				<div className="container mx-auto flex flex-col gap-5 items-center">
+					<h1 className="font-semibold text-slate-100 text-5xl">
 						Chuck Norris Jokes
 					</h1>
+
+					{/* joke display */}
+					<div
+						id="joke-display"
+						className="max-w-lg p-4 bg-slate-200/85 rounded-xl border-2 border-slate-400"
+					>
+						<p className="text-slate-900 text-center">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Aspernatur iste veritatis nobis repellendus earum dicta suscipit
+							laborum mollitia?
+						</p>
+					</div>
+
+					{/* Actions */}
+					<div id="actions-panel" className="">
+						<button
+							data-refresh-action
+							className="bg-yellow-500 py-1 px-4 text-purple-950 rounded
+							hover:bg-yellow-300"
+						>
+							Refresh
+						</button>
+					</div>
 				</div>
 			</section>
 		</main>
