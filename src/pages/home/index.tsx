0f74a10d0371520e29bbbcaf1e3eb682747b2b6c
kadeem254 <kadeemalexander254@gmail.com>
2024-06-12 22:08:21 +0300

    UPDATE: updated the HomePage component to allow passing of classname values

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,13 @@
-export default function HomePage() {
+interface HomePageParameters {
+	className?: string;
+}
+
+export default function HomePage({ className }: HomePageParameters) {
 	return (
-		<main>
+		<main className={className || ``}>
 			<section id="display-section" className="py-10">
 				<div className="container mx-auto">
-					<h1 className="font-semibold text-slate-800 text-5xl ">
+					<h1 className="font-semibold text-slate-800 text-5xl">
 						Chuck Norris Jokes
 					</h1>
 				</div>
