768f70a29945fbcf8b1dc677587fee1e64bc2a64
kadeem254 <kadeemalexander254@gmail.com>
2024-06-12 22:07:06 +0300

    UPDATE: removed background on main container, added backdrop component to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import Backdrop from "./components/backdrop";
 import HomePage from "./pages/home/index";
 
 export default function App() {
 	return (
 		<div
 			id="app-container"
-			className="w-full min-h-screen relative bg-slate-100 text-xl"
+			className="w-full min-h-screen relative text-xl font-primary"
 		>
+			<Backdrop />
 			<HomePage />
 		</div>
 	);
