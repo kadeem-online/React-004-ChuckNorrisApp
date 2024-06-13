c99334d787dee945c3cd219a979d7877760c7ae8
kadeem254 <kadeemalexander254@gmail.com>
2024-06-13 16:07:12 +0300

    UPDATE: integrated the Context Provider Wrapper component to the core app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import Backdrop from "./components/backdrop";
 import HomePage from "./pages/home/index";
+import ContextProviderWrapper from "./utils/providers";
 
 export default function App() {
 	return (
-		<div
-			id="app-container"
-			className="w-full min-h-screen relative font-primary flex flex-col
+		<ContextProviderWrapper>
+			<div
+				id="app-container"
+				className="w-full min-h-screen relative font-primary flex flex-col
 			justify-center items-center"
-		>
-			<Backdrop />
-			<HomePage />
-		</div>
+			>
+				<Backdrop />
+				<HomePage />
+			</div>
+		</ContextProviderWrapper>
 	);
 }
